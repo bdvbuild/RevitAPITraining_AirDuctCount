b738969612316dc42174054ea37011da44824cf4
Dmitriy Baranov <bdvbuild@gmail.com>
2023-01-25 11:37:25 +0400

    Add project files.

---
+++ RevitAPITraining_AirDuctCount/Main.cs
@@ -0,0 +1,33 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Mechanical;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace RevitAPITraining_AirDuctCount
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            var ducts = new FilteredElementCollector(doc)
+                .OfCategory(BuiltInCategory.OST_DuctCurves)
+                .WhereElementIsNotElementType()
+                .Cast<Duct>()
+                .ToList();
+
+            TaskDialog.Show("Количество воздуховодов", ducts.Count.ToString());
+            return Result.Succeeded;
+        }
+    }
+}
